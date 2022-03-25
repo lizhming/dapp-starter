8740f810e243b3d1094326c941b92d1358e313ea
arisac <hi@aris.ac>
2022-03-25 02:47:51 -0700

    update RPC_URL & INFURA_ID

--- src/connectors.ts
+++ src/connectors.ts
@@ -6,17 +6,15 @@ const POLLING_INTERVAL = 1000
 
 const RPC_URLS: { [chainId: number]: string } = {
   1: process.env.RPC_URL_1 as string,
-  4: process.env.RPC_URL_4 as string,
-  1007: process.env.RPC_URL_1007 as string,
-  1012: process.env.RPC_URL_1012 as string
+  4: process.env.RPC_URL_4 as string
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 4, 42, 56, 77, 97, 99, 128, 256, 1007, 1012]
+  supportedChainIds: [1, 4, 42, 56, 77, 97, 99, 128, 256]
 })
 
 export const network = new NetworkConnector({
-  urls: { 1: RPC_URLS[1], 4: RPC_URLS[4], 1007: RPC_URLS[1007], 1012: RPC_URLS[1012] },
+  urls: { 1: RPC_URLS[1], 4: RPC_URLS[4] },
   defaultChainId: 1
 })
 
