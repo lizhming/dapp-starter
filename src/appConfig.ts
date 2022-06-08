472b1a4ea7d175eb6236eb4492d02a58fff36304
arisac <hi@aris.ac>
2022-06-08 00:57:03 -0700

    update appConfig

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,9 +1,9 @@
 export const app: { [key: string]: string } = {
-  name: 'Nextjs Starter', // web app name
-  title: 'Nextjs Starter Boilerplate', // website title
-  description: 'Nextjs Starter Boilerplate by @arisac', // description of website
-  keywords: 'nextjs, react, tailwindcss, boilerplate', // keywords for SEO
-  url: 'https://nextjs-starter.aris.ac', // website url
+  name: 'dApp Starter', // web app name
+  title: 'dApp Starter Boilerplate', // website title
+  description: 'dApp Starter Boilerplate by https://github.com/arisac', // description of website
+  keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
+  url: 'https://dapp-starter.aris.ac', // website url
   image: '/icon-512x512.png', // image url for SEO
   favicon: '/favicon.ico', // favicon url
   twitter: '@arisdotac', // twitter account
