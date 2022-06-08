0d4862d390b560e9061838df5ebd4d5ddd1a200a
arisac <hi@aris.ac>
2022-06-08 00:57:38 -0700

    init dapp demo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,81 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { useRouter } from 'next/router'
 import HeadGlobal from 'components/HeadGlobal'
+// Web3Wrapper deps:
+import { getDefaultWallets, RainbowKitProvider, lightTheme, darkTheme } from '@rainbow-me/rainbowkit'
+import { Chain, chain, createClient, configureChains, WagmiConfig } from 'wagmi'
+import { infuraProvider } from 'wagmi/providers/infura'
+import { publicProvider } from 'wagmi/providers/public'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { useTheme } from 'next-themes'
+import { app } from 'appConfig'
+import { useState, useEffect } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
       <HeadGlobal />
-      <Component key={router.asPath} {...pageProps} />
+      <Web3Wrapper>
+        <Component key={router.asPath} {...pageProps} />
+      </Web3Wrapper>
     </ThemeProvider>
   )
 }
-export default MyApp
+export default App
+
+// Add Custom Chain
+const gnosisChain: Chain = {
+  id: 100,
+  name: 'Gnosis',
+  network: 'gnosis',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'xDai',
+    symbol: 'xDAI',
+  },
+  rpcUrls: {
+    default: 'http://rpc.gnosischain.com',
+  },
+  blockExplorers: {
+    default: { name: 'BlockScout', url: 'https://blockscout.com/xdai/mainnet' },
+  },
+  testnet: false,
+}
+
+// Web3Wrapper
+export function Web3Wrapper({ children }) {
+  const { chains, provider } = configureChains(
+    [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, gnosisChain],
+    [
+      infuraProvider({ infuraId: process.env.NEXT_PUBLIC_INFURA_ID !== '' && process.env.NEXT_PUBLIC_INFURA_ID }),
+      jsonRpcProvider({
+        rpc: chain => {
+          if (chain.id !== gnosisChain.id) return null
+          return { http: chain.rpcUrls.default }
+        },
+      }),
+      publicProvider(),
+    ]
+  )
+  const { connectors } = getDefaultWallets({ appName: app.name, chains })
+  const wagmiClient = createClient({ autoConnect: true, connectors, provider })
+
+  const [mounted, setMounted] = useState(false)
+  const { resolvedTheme } = useTheme()
+
+  useEffect(() => setMounted(true), [])
+  if (!mounted) return null
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider
+        chains={chains}
+        showRecentTransactions={true}
+        theme={resolvedTheme === 'dark' ? darkTheme() : lightTheme()}
+      >
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import styles from 'styles/Home.module.scss'
 import ThemeToggleButton from 'components/Theme/ThemeToggleButton'
 import ThemeToggleList from 'components/Theme/ThemeToggleList'
+import { useState } from 'react'
+import { useNetwork, useAccount, useBalance } from 'wagmi'
+import ConnectWallet from 'components/Connect/ConnectWallet'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+import { useSignMessage } from 'wagmi'
 
 export default function Home() {
   return (
@@ -31,16 +36,107 @@ function Header() {
 }
 
 function Main() {
+  const { data: account } = useAccount()
+  const { activeChain, chains, isLoading: isNetworkLoading, pendingChainId, switchNetwork } = useNetwork()
+  const { data: balance } = useBalance({
+    addressOrName: account?.address,
+  })
   return (
-    <main className={styles.main}>
-      <article className="prose dark:prose-invert md:prose-lg lg:prose-xl">
-        <h1>Heading 1</h1>
-        <h2>Heading 2</h2>
-        <h3>Heading 3</h3>
-        <h4>Heading 4</h4>
-        <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
-      </article>
+    <main className={styles.main + ' space-y-6'}>
+      <div className="text-center">
+        <p className="font-medium">Dapp Starter Boilerplate by arisac.eth</p>
+        <p>
+          <a
+            href="https://github.com/arisac/dapp-starter"
+            target="_blank"
+            className="text-sm underline"
+            rel="noreferrer"
+          >
+            https://github.com/arisac/dapp-starter
+          </a>
+        </p>
+      </div>
+
+      <div>
+        <ConnectWallet show="always" />
+      </div>
+
+      <div className="w-full max-w-xl rounded-xl bg-sky-500/10 p-6 text-center">
+        <dl className={styles.dl}>
+          <dt>Connector</dt>
+          <dd>
+            {account && account?.connector?.name}
+            {!account && (
+              <ConnectButton.Custom>
+                {({ openConnectModal }) => (
+                  <span onClick={openConnectModal} className="cursor-pointer hover:underline">
+                    Not connected, connect wallet
+                  </span>
+                )}
+              </ConnectButton.Custom>
+            )}
+          </dd>
+          <dt>Connected Network</dt>
+          <dd>{activeChain ? `${activeChain?.id}: ${activeChain?.name}` : 'n/a'}</dd>
+          <dt>Switch Network</dt>
+          <dd className="flex flex-wrap justify-center">
+            {account &&
+              chains.map(x => (
+                <button
+                  disabled={!switchNetwork || x.id === activeChain?.id}
+                  key={x.id}
+                  onClick={() => switchNetwork?.(x.id)}
+                  className={
+                    (x.id === activeChain?.id ? 'bg-green-500' : 'bg-blue-500 hover:scale-105') +
+                    ' m-1 rounded-lg py-1 px-3 text-white transition-all duration-150'
+                  }
+                >
+                  {x.name}
+                  {isNetworkLoading && pendingChainId === x.id && ' (switching)'}
+                </button>
+              ))}
+            <ConnectWallet show="disconnected" />
+          </dd>
+          <dt>Account</dt>
+          <dd className="break-all">{account ? `${account?.address}` : 'n/a'}</dd>
+          <dt>Balance</dt>
+          <dd className="break-all">{balance ? `${balance?.formatted} ${balance?.symbol}` : 'n/a'} </dd>
+          <dt>Sign Message</dt>
+          <dd className="break-all">{account ? <SignMsg /> : 'n/a'} </dd>
+        </dl>
+      </div>
     </main>
+  )
+}
+
+function SignMsg() {
+  const [msg, setMsg] = useState('Dapp Starter')
+  const { data, isError, isLoading, isSuccess, signMessage } = useSignMessage({
+    message: msg,
+  })
+  const signMsg = () => {
+    if (msg) {
+      signMessage()
+    }
+  }
+
+  return (
+    <>
+      <p>
+        <input value={msg} onChange={e => setMsg(e.target.value)} className="rounded-lg p-1" />
+        <button
+          disabled={isLoading}
+          onClick={() => signMsg()}
+          className="ml-1 rounded-lg bg-blue-500 py-1 px-2 text-white transition-all duration-150 hover:scale-105"
+        >
+          Sign
+        </button>
+      </p>
+      <p>
+        {isSuccess && <span>Signature: {data}</span>}
+        {isError && <span>Error signing message</span>}
+      </p>
+    </>
   )
 }
 
