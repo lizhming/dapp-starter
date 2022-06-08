13fcfb508f47df1e50f8bb7ee8d0276ab1708ce1
arisac <hi@aris.ac>
2022-06-08 00:56:53 -0700

    add comp: ConnectWallet

---
+++ src/components/Connect/ConnectWallet.tsx
@@ -0,0 +1,12 @@
+import { useAccount } from 'wagmi'
+import { ConnectButton } from '@rainbow-me/rainbowkit'
+
+interface Props {
+  show: 'always' | 'connected' | 'disconnected'
+}
+
+export default function ConnectWallet({ show = 'always' }: Props) {
+  const { data: account } = useAccount()
+  if ((show === 'connected' && !account?.address) || (show === 'disconnected' && account?.address)) return null
+  return <ConnectButton />
+}
